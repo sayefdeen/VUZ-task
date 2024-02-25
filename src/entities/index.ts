195d3bbfdab3ce974438bf25a6e8cf2d6aee7014
Rawad <sayefdeen.alrawad@gmail.com>
2024-02-25 05:49:06 +0300

    finish loign and register with jwt #1 and #4

---
+++ src/entities/index.ts
@@ -0,0 +1,1 @@
+export { UserSchema, User } from './users';
---
+++ src/entities/users.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { Role } from 'src/enums';
+
+@Schema()
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  fullName: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ enum: Role, default: Role.USER })
+  role: Role;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
