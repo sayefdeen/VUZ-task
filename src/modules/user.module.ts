dfe227cb9628ab05c066bd4fd04601710dda3d12
Rawad <sayefdeen.alrawad@gmail.com>
2024-02-27 00:30:21 +0300

    fix imports in user module

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from 'src/controllers';
-import { UserSchema, User } from 'src/entities';
+import { UserSchema, User, Shipment, ShipmentSchema } from 'src/entities';
 import { JwtService, UserService } from 'src/services';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: Shipment.name, schema: ShipmentSchema },
+    ]),
   ],
   controllers: [UserController],
   providers: [UserService, JwtService],
